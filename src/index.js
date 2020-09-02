3487a0f5929bf9600488bc97ada1cd8c26c25241
Keiko <24987971+webiroha@users.noreply.github.com>
2020-09-02 17:36:36 +0900

    add lint

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import './styles/style.css';
 
-const root = document.getElementById('root')
+const root = document.getElementById('root');
 
-const fragment = document.createDocumentFragment()
-const title = document.createElement('h1')
-const titleText = document.createTextNode('JavaScript Playground')
-const sub = document.createElement('p')
-const subText = document.createTextNode('This is a playground for strengthening basic JavaScript skills.')
+const fragment = document.createDocumentFragment();
+const title = document.createElement('h1');
+const titleText = document.createTextNode('JavaScript Playground');
+const sub = document.createElement('p');
+const subText = document.createTextNode(
+  'This is a playground for strengthening basic JavaScript skills.'
+);
 
-title.appendChild(titleText)
-sub.appendChild(subText)
-fragment.appendChild(title)
-fragment.appendChild(sub)
-root.appendChild(fragment)
-
+title.appendChild(titleText);
+sub.appendChild(subText);
+fragment.appendChild(title);
+fragment.appendChild(sub);
+root.appendChild(fragment);
