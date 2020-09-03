f29ca11318149c006babd12dab56bf968927c614
Keiko <24987971+webiroha@users.noreply.github.com>
2020-09-03 16:37:07 +0900

    add links between repo and demo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # playground
-This is a playground for strengthening basic JavaScript skills.
+This is a personal playground for strengthening basic JavaScript skills.
+
+Feel free to see code and demo👋🙂
+
+![JavaScript Playground image](https://github.com/webiroha/playground/blob/master/src/img/readme.jpg)
+
+here is ➡️[demo](https://webiroha.github.io/playground/)
--- src/index.js
+++ src/index.js
@@ -7,11 +7,17 @@ const title = document.createElement('h1');
 const titleText = document.createTextNode('JavaScript Playground');
 const sub = document.createElement('p');
 const subText = document.createTextNode(
-  'This is a playground for strengthening basic JavaScript skills.'
+  'This is a personal playground for strengthening basic JavaScript skills.'
 );
+const githubLink = document.createElement('a');
+const githubLinkText = document.createTextNode('See code');
+githubLink.href = 'https://github.com/webiroha/playground';
+githubLink.className = 'github-link';
 
 title.appendChild(titleText);
 sub.appendChild(subText);
+githubLink.appendChild(githubLinkText);
 fragment.appendChild(title);
 fragment.appendChild(sub);
+fragment.appendChild(githubLink);
 root.appendChild(fragment);
