98c65c977d3ec33566dde4fa9a14e11666c839eb
Keiko <24987971+webiroha@users.noreply.github.com>
2020-09-29 18:04:22 +0900

    css refactoring

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-    plugins: ['stylelint-order'],
-    extends: [
-          'stylelint-config-standard',
-          './node_modules/prettier-stylelint/config.js'
-      ],
-    ignoreFiles: [
-      '**/node_modules/**',
-    ],
-    rules: {
-      'indentation': 2,
-      'string-quotes': 'single',
-      'order/properties-alphabetical-order': true
-    },
-  };
+  plugins: ['stylelint-scss', 'stylelint-order'],
+  extends: [
+    'stylelint-config-standard',
+    './node_modules/prettier-stylelint/config.js',
+  ],
+  ignoreFiles: ['**/node_modules/**'],
+  rules: {
+    indentation: 2,
+    'string-quotes': 'single',
+    'order/properties-alphabetical-order': true,
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+  },
+};
